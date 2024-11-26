07a42e2d38651ce3855daadb42a13f4a65c4b622
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-26 22:55:10 +0100

    feat: :art: add custom tailwind colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
@@ -9,10 +9,52 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // foreground
+        foreground: {
+          500: "#667085",
+        },
+        // background
+        bg: {
+          primary: "#FFFFFF",
+          secondary: "#F9FAFB",
+        },
+        // border
+        border: {
+          primary: "#D0D5DD",
+          secondary: "#EAECF0",
+        },
+        // text
+        text: {
+          primary: {
+            900: "#101828",
+          },
+          secondary: {
+            700: "#344054",
+          },
+          tertiary: {
+            600: "#475467",
+          },
+          placeholder: "#667085",
+        },
+        // button
+        button: {
+          primary: {
+            bg: "#7F56D9",
+            fg: "#FFFFFF",
+            border: "#7F56D9",
+            tertiary: "#475467",
+          },
+          secondary: {
+            bg: "#FFFFFF",
+            border: "#D0D5DD",
+            fg: "#344054",
+            colorBg: "#FFFFFF",
+            colorBorder: "#D6BBFB",
+            colorFg: "#6941C6",
+          },
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
