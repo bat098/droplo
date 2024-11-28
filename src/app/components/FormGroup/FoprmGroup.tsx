aaefd5774d8a334914881925b8431ade16ec6594
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-28 21:45:47 +0100

    refactor: rename file from FoprmGroup to FormGroup

--- src/app/components/FormGroup/FoprmGroup.tsx
+++ src/app/components/FormGroup/FoprmGroup.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import { FormGroupInterface } from "./FormGroup"
+import { FormGroupInterface } from "./FormGroup.types"
 
-const FoprmGroup = (props: FormGroupInterface) => {
+const FormGroup = (props: FormGroupInterface) => {
   const { children } = props
   return <div className="flex flex-col gap-1.5">{children}</div>
 }
 
-export default FoprmGroup
+export default FormGroup
