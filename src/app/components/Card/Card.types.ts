4918e88113707e3f494586b551d2aef203f16729
Mateusz Baciak <m.baciak@muchmore.pl>
2024-11-30 00:21:04 +0100

    feat: :sparkles: add Card component

---
+++ src/app/components/Card/Card.types.ts
@@ -0,0 +1,3 @@
+export interface Cardinterface {
+  children: React.ReactNode
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client"
 
-import ButtonGroup from "./components/ButtonGroup/ButtonGroup"
-import Button from "./components/Button/Button"
+import Card from "./components/Card/Card"
 
 export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <ButtonGroup>
-        <Button variant="outlined">Option 1</Button>
-        <Button variant="outlined">Option 2</Button>
-        <Button variant="outlined">Option 3</Button>
-        <Button variant="outlined">Option 4</Button>
-        <Button variant="outlined">Option 5</Button>
-      </ButtonGroup>
+      <Card>test</Card>
     </div>
   )
 }
