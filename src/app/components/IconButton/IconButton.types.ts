3737d5e0a8f6c40980f72a902494292d6c3f7710
Mateusz Baciak <m.baciak@muchmore.pl>
2024-12-02 18:52:46 +0100

    feat: add default button type for IconButton

--- src/app/components/IconButton/IconButton.types.ts
+++ src/app/components/IconButton/IconButton.types.ts
@@ -1,7 +1,10 @@
+import { ButtonType } from "../Button/Button.types"
+
 export interface IconButtonInterface {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
   disabled?: boolean
   children?: React.ReactNode
   className?: string
   innerRef?: React.RefObject<HTMLButtonElement>
+  type: ButtonType
 }
