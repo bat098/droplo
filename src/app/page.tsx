c097546f76fc3bec8c395e4a85274eb794dd73d4
Mateusz Baciak <m.baciak@muchmore.pl>
2024-12-02 18:23:35 +0100

    feat: add Form Componenet (without logic)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Container from "@/app/components/Container/Container"
-import { SortableTree } from "./components/SortableTree/SortableTree"
+// import { SortableTree } from "./components/SortableTree/SortableTree"
+import Form from "./components/Form/Form"
 
 export default function Home() {
   return (
     <div>
       <Container>
-        <SortableTree indicator removable editable />
+        <Form />
+        {/* <SortableTree indicator removable editable /> */}
       </Container>
     </div>
   )
